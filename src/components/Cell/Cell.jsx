84c57627263950570f490f680be6197ca2d212e5
Niko <nicoym23@gmail.com>
2024-12-13 18:21:10 +0700

    rework key

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -3,8 +3,8 @@ import classes from "./Cell.module.css";
 const Cell = ({ word }) => {
     return (
         <div className={classes.block}>
-            {word.split('').map(c => c.codePointAt(0).toString(2)).join('').split('').map((value) => (
-                <div key={Math.random()}
+            {word.split('').map(c => c.codePointAt(0).toString(2)).join('').split('').map((value, index) => (
+                <div key={index}
                     className={value === '0' ? classes.empty : classes.cell}
                 />
             ))}
